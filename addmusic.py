669c1580d9f87ad59304bb5db7716585e7c317d3
Genevieve <genevieve.okon@gmail.com>
2021-01-02 05:13:51 -0500

    create playlist, check if play list exists already and search for a track added to code

--- addmusic.py
+++ addmusic.py
@@ -1,19 +1,23 @@
 # 1. Log into youtube
 # 2. get all liked music videos
-# 3. log into apple music
-# 4. create a new playlist
-# 5. search for song
-# 6. add to playlist
+# 3. create a new playlist
+# 4. search for song
+# 5. add to playlist
+
 import requests
-from secretinfo import userid
-from secretinfo import authtoken
+import datetime
+import json
+from secretinfo import userid,authtoken
+
 class AddMusic():
 
     # init method or constructor
 
 
     def __init__(self):
-       pass
+       self.date = datetime.datetime.now();
+       self.track = "hello"
+       self.artist = "jorja"
 
 
     def access_youtube(self):
@@ -22,19 +26,53 @@ class AddMusic():
     def get_liked_music(self):
         pass
     
+    #check if playlist name already exists
+    def doesplaylistexist(self,name):
+        url = 'https://api.spotify.com/v1/users/{}/playlists'.format(userid)
+        response = requests.get(url, headers = {"Content-Type": "application/json", "Authorization":"Bearer {}".format(authtoken)})
+        total=response.json()["total"]
+        for i in range(0,int(total)):
+            if response.json()["items"][i]["name"] == name:
+                a= True
+                self.playlistid = response.json()["items"][i]["id"]
+                break
+            else:
+                a= False
+        return a
     
-    def access_apple_music(self):
-        print(authtoken)
-        #response = requests.get("https://api.spotify.com/v1/users/12160778651/playlists" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQBI3nsOXxRRt-8_n7sQzysl1opN9xIIJ1VdUgyuJlAEf7xUgAZI8a_ssOlEFNRcDQTMu7HlKIxix_1dLAPtF6nIR0HSLoTpVGaGQRG44z4nZZK4Kd6UmsJroqfGrxIQhFiZfcnn8zpRX0GKamjeilL8ArKJWuyFmP6VfBYD5t2hIisPlkegWrE56_iqhybO2Oj0OqIBfvVfgcnLWsXA3Pjgq7b_z6J2Tf7sdw")
-        
-        #print(response.status_code)
-        
-
-
+    #create new playlist or get existing playlist id
+    #generates playlist name by date e.g. New Playlist Jan-02-2021
     def create_playlist(self):
-        pass
+        
+        playlistname = "New Playlist "+str(date.strftime("%B")[0:3])+"-"+str(date.strftime("%d"))+"-"+str(date.year)
+        if able.doesplaylistexist(playlistname):
+            playlistid = able.playlistid
+            print("playlist already exists ")
+            #print("playlist id: "+str(able.playlistid))
+        else:
+            url = 'https://api.spotify.com/v1/users/{}/playlists'.format(userid)
+            obj = json.dumps({"name": playlistname,
+            "description": "New playlist description",
+            "public": "false"})
+            print("playlist does not exist, creating new playlist")
+            response = requests.post(url, data = obj, headers = {"Content-Type": "application/json", "Authorization":"Bearer {}".format(authtoken)})
+            playlistid = response.json()["id"]
+            #print("new playlist id: "+str(playlistid))
+        
+        return playlistid
+        
 
-    def search_add(self):
-        pass
+
+
+    def search_add(self,track):
+        
+        url = 'https://api.spotify.com/v1/search?q={}&type=track&market=US&limit=20&offset=0'.format(track)
+        response = requests.get(url, headers = {"Content-Type": "application/json", "Authorization":"Bearer {}".format(authtoken)})
+        print(url)
+        uri = response.json()["tracks"]["items"][0]["uri"] #just first song
+        print(uri)
+        
 able = AddMusic()
-able.access_apple_music()
+date = able.date
+able.search_add(able.track)
+able.create_playlist()
